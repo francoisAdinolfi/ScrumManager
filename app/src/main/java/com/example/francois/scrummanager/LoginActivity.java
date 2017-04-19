89ccaf68a28d6c921fc7acfcfbaa602721a1fd83
francoisAdinolfi <francois.adinolfi@etu.univ-amu.fr>
2017-04-19 13:02:39 +0200

    login/register
    liaison entre les 2 activités

--- app/src/main/java/com/example/francois/scrummanager/LoginActivity.java
+++ app/src/main/java/com/example/francois/scrummanager/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.francois.scrummanager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -64,7 +65,8 @@ public class LoginActivity extends Activity {
     private OnClickListener inscriptionListener = new OnClickListener() {
         @Override
         public void onClick(View v) {
-
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
         }
     };
 }
