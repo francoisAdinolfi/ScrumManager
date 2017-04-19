cf9721e3f72daa699bfb0ef99734abace65e0cbd
brossenicolas <nicolas.brosse@etu.univ-amu.fr>
2017-04-19 13:35:13 +0200

    register

--- app/src/main/java/com/example/francois/scrummanager/RegisterActivity.java
+++ app/src/main/java/com/example/francois/scrummanager/RegisterActivity.java
@@ -5,33 +5,99 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String REGISTER_URL = "http://scrummaster.pe.hu/user.php";
+    private Button btnRegister;
+    private TextView linkLogin;
+    private EditText inputName;
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private RadioGroup radioGroup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        final Button registerButton = (Button) findViewById(R.id.buttonRegister);
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        inputName = (EditText) findViewById(R.id.name);
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        linkLogin = (TextView) findViewById(R.id.linkLogin);
+        radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                register();
+                String name = inputName.getText().toString().trim();
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+                final int role = radioGroup.getCheckedRadioButtonId();
+                if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Inputs must be filled", Toast.LENGTH_LONG).show();
+                } else if (role == -1) {
+                    Toast.makeText(getApplicationContext(), "A role must be checked", Toast.LENGTH_LONG).show();;
+                } else {
+                    register(name, email, password, role, REGISTER_URL);
+                }
             }
         });
 
-        final TextView loginLink = (TextView) findViewById(R.id.linkLogin);
-        loginLink.setOnClickListener(new View.OnClickListener() {
+        linkLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
 
-    public void register() {
-
+    public void register(final String name, final String email, final String password, final int role, String url) {
+        final StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(RegisterActivity.this, response, Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(RegisterActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", name);
+                params.put("email", email);
+                params.put("password", password);
+                if(role  == R.id.radioScrumMaster){
+                    params.put("role", "1");
+                }
+                else if(role  == R.id.radioDeveloper){
+                    params.put("role", "2");
+                }
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
