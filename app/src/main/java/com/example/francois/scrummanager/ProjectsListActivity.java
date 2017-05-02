83c3716a0c079e1c6f783b9a550a65c7e4c4c856
francoisAdinolfi <francois.adinolfi@etu.univ-amu.fr>
2017-05-03 01:53:43 +0200

    ajout taskList

--- app/src/main/java/com/example/francois/scrummanager/ProjectsListActivity.java
+++ app/src/main/java/com/example/francois/scrummanager/ProjectsListActivity.java
@@ -7,9 +7,12 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -31,7 +34,8 @@ public class ProjectsListActivity extends AppCompatActivity {
     private static final String PROJECT_URL = "http://scrummaster.pe.hu/project.php";
     private SessionManager session;
     private ListView listProjects;
-    private ArrayList<String> projects = new ArrayList<String>();
+    private ArrayList<String> projects = new ArrayList<>();
+    private ArrayList<Integer> idProjects = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,9 +62,10 @@ public class ProjectsListActivity extends AppCompatActivity {
                             for (int i = 0; i < j.length(); i++) {
                                 JSONObject JOStuff = j.getJSONObject(i);
                                 projects.add(JOStuff.getString("name"));
+                                idProjects.add(JOStuff.getInt("id_project"));
                             }
 
-                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(ProjectsListActivity.this, android.R.layout.simple_list_item_1, projects);
+                            ArrayAdapter<String> adapter = new ArrayAdapter<>(ProjectsListActivity.this, android.R.layout.simple_list_item_1, projects);
                             listProjects.setAdapter(adapter);
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -75,7 +80,7 @@ public class ProjectsListActivity extends AppCompatActivity {
                 }) {
             @Override
             protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
+                Map<String, String> params = new HashMap<>();
                 if(user.get(SessionManager.KEY_ROLE).equals("scrummaster"))
                     params.put("tag", "scrummasterprojects");
                 else
@@ -86,6 +91,17 @@ public class ProjectsListActivity extends AppCompatActivity {
         };
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
+
+        listProjects.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int idprojet = idProjects.get(projects.indexOf(((TextView) view).getText()));
+                Intent intent = new Intent(ProjectsListActivity.this, TaskProjetsListActivity.class);
+                intent.putExtra("idProjet",idprojet);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     @Override
@@ -110,4 +126,6 @@ public class ProjectsListActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
---
+++ app/src/main/java/com/example/francois/scrummanager/TaskProjetsListActivity.java
@@ -0,0 +1,115 @@
+package com.example.francois.scrummanager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TaskProjetsListActivity extends AppCompatActivity {
+
+    private static final String PROJECT_URL = "http://scrummaster.pe.hu/project.php";
+    private SessionManager session;
+    private ListView taskList;
+    private int idProjet;
+    private ArrayList<String> tasks = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projectslist);
+
+        session = new SessionManager(getApplicationContext());
+        session.checkLogin();
+        final HashMap<String, String> user = session.getUserDetails();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("TaskList :");
+        setSupportActionBar(toolbar);
+
+        idProjet = getIntent().getIntExtra("idProjet",0);
+
+        taskList = (ListView) findViewById(R.id.taskList);
+
+        final StringRequest stringRequest = new StringRequest(Request.Method.POST, PROJECT_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray j = new JSONArray(response);
+
+                            for (int i = 0; i < j.length(); i++) {
+                                JSONObject JOStuff = j.getJSONObject(i);
+                                tasks.add(JOStuff.getString("name"));
+                            }
+
+                            ArrayAdapter<String> adapter = new ArrayAdapter<>(TaskProjetsListActivity.this, android.R.layout.simple_list_item_2, tasks);
+                            taskList.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(TaskProjetsListActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("tag", "scrummastertasks");
+                params.put("id_projet", Integer.toString(idProjet));
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        if(!session.getUserDetails().get(SessionManager.KEY_ROLE).equals("scrummaster")){
+            menu.findItem(R.id.action_addproject).setVisible(false);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_logout:
+                session.logoutUser();
+                return true;
+            case R.id.action_addproject:
+                startActivity(new Intent(TaskProjetsListActivity.this, AddProjectActivity.class));
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
