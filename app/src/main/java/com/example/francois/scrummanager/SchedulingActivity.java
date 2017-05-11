35a1a22a97def5a2a27030f8fde12eeda52a5417
brossenicolas <nicolas.brosse@etu.univ-amu.fr>
2017-05-11 18:33:22 +0200

    choco

--- app/src/main/java/com/example/francois/scrummanager/SchedulingActivity.java
+++ app/src/main/java/com/example/francois/scrummanager/SchedulingActivity.java
@@ -15,6 +15,9 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solution;
+import org.chocosolver.solver.variables.IntVar;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -103,6 +106,24 @@ public class SchedulingActivity extends AppCompatActivity {
         };
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
+
+        int n = 8;
+        Model model = new Model(n + "-queens problem");
+        IntVar[] vars = new IntVar[n];
+        for(int q = 0; q < n; q++){
+            vars[q] = model.intVar("Q_"+q, 1, n);
+        }
+        for(int i  = 0; i < n-1; i++){
+            for(int j = i + 1; j < n; j++){
+                model.arithm(vars[i], "!=",vars[j]).post();
+                model.arithm(vars[i], "!=", vars[j], "-", j - i).post();
+                model.arithm(vars[i], "!=", vars[j], "+", j - i).post();
+            }
+        }
+        Solution solution = model.getSolver().findSolution();
+        if(solution != null){
+            Toast.makeText(SchedulingActivity.this, solution.toString(), Toast.LENGTH_LONG).show();
+        }
     }
 
     public boolean onSupportNavigateUp() {
